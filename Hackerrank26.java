b9fc46b130d5b76445ce1731a2fe0e25c400de1d
Pranay Kumar Reddy Goraka <63572862+Pranay2510@users.noreply.github.com>
2021-02-02 20:57:55 +0530

    Code 215 Day 69

---
+++ Hackerrank26.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Hackerrank26 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner (System.in);
+            try
+            {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                System.out.println(a/b);
+            }
+        catch(InputMismatchException e)
+        {
+            System.out.println(e.getClass().getName());
+        }
+        catch(ArithmeticException e1)
+        {
+            System.out.println(e1.getClass().getName()+": / by zero");
+        }       
+    }    
+}
